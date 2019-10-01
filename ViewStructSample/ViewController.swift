f13a9c43eefede40a72a418d1f7feba960e32bd1
kvoyant <kvoyant@gmail.com>
2019-10-01 15:26:24 +0900

    init

---
+++ ViewStructSample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ViewStructSample
+//
+//  Created by APPLE on 01/10/2019.
+//  Copyright © 2019 yhkim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

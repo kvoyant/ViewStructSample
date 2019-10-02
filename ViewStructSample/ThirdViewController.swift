0eb1b295b4d90e4cbac9af6cccdce64c6767bd5a
kvoyant <kvoyant@gmail.com>
2019-10-02 12:26:41 +0900

    네비게이션 컨트롤러 함수들 정리(주석)

--- ViewStructSample/ThirdViewController.swift
+++ ViewStructSample/ThirdViewController.swift
@@ -13,6 +13,18 @@ class ThirdViewController: UIViewController {
     }
     
     @IBAction func onBtnBack(_ sender: Any) {
-//        self.navigationController?.popViewController(animated: true)
+        self.navigationController?.popViewController(animated: true)
+        
+        //네비게이션 컨트롤러 함수들(뷰를 관리해 줌)
+        //뒤로가기
+        //        self.navigationController?.popViewController
+        //맨처음 뷰로 이동
+        //        self.navigationController?.popToRootViewController
+        //특정 뷰로 이동
+        //        self.navigationController?.popToViewController
+        //특정 뷰를 삭제
+        //        self.navigationController?.viewControllers에서 삭제
+        //중간에 뷰 추가
+        //        self.navigationController?.viewControllers에서 추가
     }
 }
